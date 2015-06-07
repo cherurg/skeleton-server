9d91ef0b46a865c7885a6075feb14f09c9f2a76e
Alexey Karpov <karpovad@yandex.ru>
2015-06-08 02:34:54 +0300

    express with headers

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var server = require('http').createServer();
+/*var server = require('http').createServer();
 var io = require('socket.io')(server);
 
 io.on('connection', function (socket) {
@@ -11,6 +11,37 @@ io.on('connection', function (socket) {
   socket.on('disconnect', function () {
     console.log('disconnected');
   });
+});
+
+server.listen(80);*/
+
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+app.disable('x-powered-by');
+app.use(function (req, res, next) {
+        res.setHeader('Access-Control-Allow-Origin', "http://"+req.headers.host+':8000');
+
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        next();
+    }
+);
+
+io.on('connection', function (socket) {
+
+  socket.on('data', function (data) {
+    socket.broadcast.emit('data', data);
+  });
+
+  socket.on('disconnect', function () {
+    
+  });
+
+  socket.send('yes?');
+
 });
 
 server.listen(80);
