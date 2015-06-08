734f14d430cf8ba7e33cc8f8af4e9fe55575ac33
Alexey Karpov <karpovad@yandex.ru>
2015-06-09 00:03:47 +0300

    8000

--- index.js
+++ index.js
@@ -18,20 +18,20 @@ server.listen(80);*/
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
-var io = require('socket.io')(server, {
+var io = require('socket.io')(server/*, {
 	origins:'visualmath.ru:* http://visualmath.ru:* http://www.visualmath.ru:* www.visualmath.ru:*'
-});
+}*/);
 
 app.disable('x-powered-by');
 /*app.use(express.static('public'));*/
-app.use(function (req, res, next) {
+/*app.use(function (req, res, next) {
         res.setHeader('Access-Control-Allow-Origin', "http://"+req.headers.host+':8000');
 
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
         res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
         next();
     }
-);
+);*/
 
 io.on('connection', function (socket) {
 
